8cfb576d15d987a94a5b78638e399b2221ac9beb
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-04-08 20:07:07 +0800

    fixed miscalculation of platform width in classes.py

--- assets/classes.py
+++ assets/classes.py
@@ -243,7 +243,7 @@ class Platform:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = 30
+        self.width = 20
         self.height = 10
 
     def draw(self, screen, colour):
